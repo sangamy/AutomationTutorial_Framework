0126c676f8ff27952cf47a8d80dfc5bea4c0bfe3
Sangam Yadagiri <sangam.yadagiri@gmail.com>
2019-03-10 20:04:26 -0400

    Automation Framework - Utils

--- AutomationTutorial/src/utils/ReportingUtils.java
+++ AutomationTutorial/src/utils/ReportingUtils.java
@@ -1,8 +1,10 @@
 package utils;
 
+import data.Constants;
+
 public class ReportingUtils {
 	
-	public static void reportResult (String reportComment, String expectedValue, String actualValue){
+	public static void reportSimple (String reportComment, String expectedValue, String actualValue){
 		if(expectedValue.equals(actualValue))
 			System.out.println("Pass - "+reportComment + ": Expected = "+expectedValue+ " & Actual = "+actualValue);
 		else
@@ -10,12 +12,43 @@ public class ReportingUtils {
 	
 	}
 
-	public static void reportResult (String reportComment, int expectedValue, int actualValue){
+	public static void reportSimple (String reportComment, int expectedValue, int actualValue){
 		if(expectedValue == actualValue)
 			System.out.println("Pass - "+reportComment + ": Expected = "+expectedValue+ " & Actual = "+actualValue);
 		else
 			System.out.println("Fail - "+reportComment + ": Expected = "+expectedValue+ " is NOT same as Actual = "+actualValue);
 	
+	}
+	
+
+
+
+	public static void reportResult(String verdict_Pass_Fail, String header, String reportComment){
+
+		int counter = Constants.counter;
+		Constants.counter++;
+
+		switch (verdict_Pass_Fail) {
+		case "Pass":
+			System.out.println("PASS ~ Test Step Passed for "+header+ " :: "+ reportComment);
+			PropertyUtils.propertyFile_Write(Constants.screenshotFolderPath+"Captions.properties", "Caption"+counter, reportComment);
+			ScreenshotUtils.screenshot(Constants.screenshotFolderPath, counter);
+			break;
+		case "Fail":
+			System.out.println("FAIL ~ Test Step FAILED for "+header+ " :: "+ reportComment);
+			PropertyUtils.propertyFile_Write(Constants.screenshotFolderPath+"Captions.properties", "Caption"+counter, reportComment);
+			ScreenshotUtils.screenshot(Constants.screenshotFolderPath, counter);
+			break;
+		case "Done":
+			System.out.println("Done ~ Test Step is Done for "+header+ " :: "+ reportComment);
+			PropertyUtils.propertyFile_Write(Constants.screenshotFolderPath+"Captions.properties", "Caption"+counter, reportComment);
+			ScreenshotUtils.screenshot(Constants.screenshotFolderPath, counter);
+			break;
+		default:
+			System.out.println("Please mention either Pass, Fail or Done");
+			break;
+		}
+
 	}
 
 }
